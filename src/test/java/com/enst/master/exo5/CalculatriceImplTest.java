23b56d1bc990dba790478ad9ba82ef483a1d0b43
Etienne Perrineau <m2com6469@scapin.enst.fr>
2014-12-09 12:46:04 +0100

    delete code for students

--- src/test/java/com/enst/master/exo5/CalculatriceImplTest.java
+++ src/test/java/com/enst/master/exo5/CalculatriceImplTest.java
@@ -21,35 +21,19 @@ public class CalculatriceImplTest {
 
 	@Test
 	public void testAdd() {
-		long result = calculatrice.add(1, 2);
-		Assert.assertEquals(3, result);
 	}
 
 	@Test
 	public void testSubstract() {
-		long result = calculatrice.substract(4, 2);
-		Assert.assertEquals(2, result);
 	}
 	
 	
 	@Test
 	public void testMultiply() {
 		// execute the method
-		long result = calculatrice.multiply(2, 2);
 		
 		//assert that the result is good
-		Assert.assertEquals(4, result);
-	}	
-
-	@Test
-	public void testMultiplyWithVerify() {
-
-		// execute the method
-		long result = calculatrice.multiply(2, 2);
-		
-		//assert that the result is good
-		Assert.assertEquals(4, result);
-
+		//Assert.assertEquals ... the rest of your code;
 	}	
 	
 	
@@ -57,23 +41,18 @@ public class CalculatriceImplTest {
 	@Test
 	public void testDivide() {
 
-		// execute the method
-		long result = calculatrice.divide(2, 2);
-		
-		//assert that the result is good
-		Assert.assertEquals(1, result);
 	}	
 	
 	
 	/*
 	 * Before JUnit 4.7 version - just one step in the test
 	 */
-	@Test(expected = IllegalArgumentException.class)
+	//@Test(... something to add here)
 	public void testIllegalArgument() throws Exception {
 		calculatrice.add(-1, 1);
 	}
 
-	@Test(expected = IllegalArgumentException.class)
+	//@Test(... something to add here)
 	public void testDivideByZero() {
 		calculatrice.divide(2, 0);
 	}	
@@ -84,23 +63,16 @@ public class CalculatriceImplTest {
 	 * After JUnit 4.7 version - step 1
 	 */	
 	
-	@Rule public ExpectedException thrown = ExpectedException.none();		
+	// ... create @Rule Annotated code		
 	
 	/*
 	 * After JUnit 4.7 version - step 2
 	 */
-	@Test
+
 	public void testAddNegativeFigureExpectedException() throws Exception {
-		thrown.expect(IllegalArgumentException.class);
-		thrown.expectMessage(CalculatriceImpl.ERR_ARG_CANNOT_BE_NEGATIVE);
-		calculatrice.add(1L, -1L);
 	}
 	
-	@Test
 	public void testDivideByZeroExpectedException() throws Exception {
-		thrown.expect(IllegalArgumentException.class);
-		thrown.expectMessage(CalculatriceImpl.ERR_ARG_CANNOT_BE_NULL);
-		calculatrice.divide(1, 0);
 	}	
 	
 
